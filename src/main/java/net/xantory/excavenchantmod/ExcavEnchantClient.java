d84cb47274c98f12c2cfc05c93e722c21877e328
Xantory <paul.jawa.pp@gmail.com>
2024-12-28 16:23:27 +0100

    Initial commit

---
+++ src/main/java/net/xantory/excavenchantmod/ExcavEnchantClient.java
@@ -0,0 +1,10 @@
+package net.xantory.excavenchantmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExcavEnchantClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
